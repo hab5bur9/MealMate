7da5771e38518736bbbbc37098a4bd9525d49401
hab5bur9 <117581838+hab5bur9@users.noreply.github.com>
2023-05-28 17:42:11 +0900

    Food page complete

--- MealMate/app/src/main/java/com/anonymous/mealmate/viewmodel/FoodViewModel.java
+++ MealMate/app/src/main/java/com/anonymous/mealmate/viewmodel/FoodViewModel.java
@@ -1,11 +1,17 @@
 package com.anonymous.mealmate.viewmodel;
 
 import android.app.Application;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.databinding.BindingAdapter;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.anonymous.mealmate.R;
 import com.anonymous.mealmate.model.entity.Food;
 import com.anonymous.mealmate.model.repository.FoodRepository;
 
@@ -37,4 +43,48 @@ public class FoodViewModel extends AndroidViewModel {
 
     public void setClickedFood(Food food) {clickedFood.setValue(food);}
     public LiveData<Food> getClickedFood() {return clickedFood;}
+
+
+
+    //xml binding method, do not erase
+    //binding Text complete, enable to user
+    public void onSearchFood(View view, String inputFoodName){
+        //Food Name 전달 받아 repository에 데이터 요청하는 메소드
+        //db search logic
+        //TODO 레파지토리에 food 검색 기능 연결
+        //foodRepository.searchFood(foodName);
+
+
+        //test
+        Toast.makeText(view.getContext(), inputFoodName, Toast.LENGTH_SHORT).show();
+        foodRepository.insertFood(new Food(inputFoodName,300,200,1000,444));
+        //String foodName = ((EditText)view).getText().toString();
+    }
+
+    //xml binding method, do not erase
+    public void onSelectedLikeFood(View view){
+
+    }
+
+    //xml binding method, do not erase
+    public void onLikeStateChange(View view,Food food){
+        //Food Adapter에 하트 버튼을 눌렀을때 Food db를 업데이트 해주는 메서드
+        //changed 1 - > true - > Full , 0 -> false -> Empty
+        int currentState = food.getFoodLike();
+        Boolean state  = (currentState ==1 );
+        //likeImageStateChanged(view, state);
+
+        //db upload
+        foodRepository.updateFood(food);
+
+        //test
+    }
+
+    public void onAddFood(View view){
+
+    }
+//    @BindingAdapter("app:likeStateChanged")
+//    public static void likeImageStateChanged(View view,Boolean changed){
+//        view.setBackgroundResource(changed ? R.drawable.btn_heart_full : R.drawable.btn_heart_empty);
+//    }
 }
