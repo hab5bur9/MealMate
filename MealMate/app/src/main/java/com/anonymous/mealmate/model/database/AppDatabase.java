1865668e65899ccb587653892b99bde999921b75
jiyoonOk <ojoj7ojo@gmail.com>
2023-05-29 15:58:13 +0900

    MealFood 및 Meal 관련 Dao/Repository/ViewModel 수정 필요.
    API 관련 코드는 완성형

--- MealMate/app/src/main/java/com/anonymous/mealmate/model/database/AppDatabase.java
+++ MealMate/app/src/main/java/com/anonymous/mealmate/model/database/AppDatabase.java
@@ -1,5 +1,7 @@
 package com.anonymous.mealmate.model.database;
 
+import static com.anonymous.mealmate.model.constants.Constants.DATABASE_NAME;
+
 import android.content.Context;
 
 import androidx.annotation.NonNull;
@@ -11,37 +13,48 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.anonymous.mealmate.model.dao.FoodDao;
 import com.anonymous.mealmate.model.dao.MealDao;
+import com.anonymous.mealmate.model.dao.MealFoodDao;
 import com.anonymous.mealmate.model.dao.UserDao;
 import com.anonymous.mealmate.model.entity.Food;
 import com.anonymous.mealmate.model.entity.Meal;
+import com.anonymous.mealmate.model.entity.MealFood;
 import com.anonymous.mealmate.model.entity.User;
 
-// version = 1 : 처음 생성할 때 버전 1로 생성. 이후에 업데이트할 때마다 버전을 올려줘야 함.
-@Database(entities = {Meal.class, Food.class, User.class}, version = 2)
+// version = 1 : MealFood 테이블 추가 및 Meal 테이블 수정에 따른 버전 업
+@Database(entities = {Meal.class, Food.class, User.class, MealFood.class}, version = 1, exportSchema = true)
 public abstract class AppDatabase extends RoomDatabase {
 
-    private static final String DATABASE_NAME = "app_database";
     private static AppDatabase instance;
 
-    public abstract MealDao mealDao();      // MealDao를 사용하기 위한 메소드.
+    public abstract MealDao mealDao();
     public abstract FoodDao foodDao();
     public abstract UserDao userDao();
+    public abstract MealFoodDao mealFoodDao();
 
     public static synchronized AppDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                             AppDatabase.class, DATABASE_NAME)
-                    .addMigrations(MIGRATION_1_2)
+                    .fallbackToDestructiveMigration()
                     .build();
         }
         return instance;
     }
 
-    private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+    /*private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
-            // food 테이블에 foodLike 컬럼을 추가.
-            database.execSQL("ALTER TABLE food ADD COLUMN foodLike INTEGER DEFAULT 0");
+            // MealFood 테이블을 추가합니다.
+            database.execSQL("CREATE TABLE mealFood (mealIndex INTEGER, foodIndex INTEGER, mealFoodAmount INTEGER, checked INTEGER, " +
+                    "PRIMARY KEY(mealIndex, foodIndex)," +
+                    "FOREIGN KEY(mealIndex) REFERENCES meal(mealIndex)," +
+                    "FOREIGN KEY(foodIndex) REFERENCES food(foodIndex))");
+
+            // Meal 테이블에서 더 이상 사용하지 않는 칼럼을 제거합니다.
+            database.execSQL("CREATE TABLE new_meal (mealIndex INTEGER PRIMARY KEY NOT NULL, mealDate TEXT, mealCnt INTEGER)");
+            database.execSQL("INSERT INTO new_meal (mealIndex, mealDate, mealCnt) SELECT mealIndex, mealDate, mealCnt FROM meal");
+            database.execSQL("DROP TABLE meal");
+            database.execSQL("ALTER TABLE new_meal RENAME TO meal");
         }
-    };
+    };*/
 }
