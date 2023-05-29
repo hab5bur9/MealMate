1ce647e53680e100fd14bca520a8e128fc6ca148
JiHwan99 <117926985+JiHwan99@users.noreply.github.com>
2023-05-29 09:51:49 +0900

    .

--- MealMate/app/src/main/java/com/anonymous/mealmate/view/fragment/ChartFragment.java
+++ MealMate/app/src/main/java/com/anonymous/mealmate/view/fragment/ChartFragment.java
@@ -1,19 +1,15 @@
 package com.anonymous.mealmate.view.fragment;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.WebView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import com.anonymous.mealmate.R;
+import androidx.appcompat.app.AppCompatActivity;
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
@@ -23,77 +19,93 @@ import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class ChartFragment extends Fragment {
-private BarChart barChart;
+public class ChartFragment extends AppCompatActivity {
 
-    @Nullable
+    private BarChart barChart;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_chart);
 
-       View view = inflater.inflate(R.layout.fragment_chart, container, false);
+        barChart = findViewById(R.id.chart);
+        createBarChart();
+    }
 
-        // 데이터 리스트 생성 (각 요일의 완료도 퍼센트)
-        // 완료도를 나타내는 값을 저장하기 위한 Float 타입의 리스트입니다. 각각의 값은 해당 날짜의 완료도를 나타냅니다.
+    private void createBarChart() {
+
+        List<String> daysOfWeek = new ArrayList<>();
+        daysOfWeek.add("일");
+        daysOfWeek.add("월");
+        daysOfWeek.add("화");
+        daysOfWeek.add("수");
+        daysOfWeek.add("목");
+        daysOfWeek.add("금");
+        daysOfWeek.add("토");
+
+
         List<Float> completionPercentages = new ArrayList<>();
-        completionPercentages.add(90f);
-        completionPercentages.add(80f);
-        completionPercentages.add(70f);
-        completionPercentages.add(60f);
-        completionPercentages.add(50f);
-        completionPercentages.add(40f);
-        completionPercentages.add(30f);
-        // ...
+        completionPercentages.add(100f);
+        completionPercentages.add(100f);
+        completionPercentages.add(100f);
+        completionPercentages.add(100f);
+        completionPercentages.add(100f);
+        completionPercentages.add(100f);
+        completionPercentages.add(100f);
 
-        // BarEntry 리스트 생성
+
         List<BarEntry> entries = new ArrayList<>();
         for (int i = 0; i < completionPercentages.size(); i++) {
             float completionPercentage = completionPercentages.get(i);
-            entries.add(new BarEntry(i, completionPercentage));//값을 넣어서 막대 그래프로 표시
+            entries.add(new BarEntry(i, completePercent()));
         }
 
-        // BarDataSet 생성
-        BarDataSet dataSet = new BarDataSet(entries, "Completion");
-        dataSet.setColor(Color.rgb(208,112,251));
+        BarDataSet dataSet = new BarDataSet(entries, "완료도");
+        dataSet.setColor(Color.rgb(208, 112, 251));
+        dataSet.setDrawValues(false);
 
-        // BarData 생성
         BarData barData = new BarData(dataSet);
-        barData.setBarWidth(0.5f);
 
-        // X축 설정 (일주일 요일 표시)
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setValueFormatter(new ValueFormatter() {
-            private final String[] days = new String[]{"일", "월", "화", "수", "목", "금", "토"};
+        // 막대의 너비 조정
+        float barWidth = 0.4f; // 막대의 너비 설정
+        barData.setBarWidth(barWidth);
 
-            @Override
-            public String getAxisLabel(float value, AxisBase axis) {
-                int index = (int) value;
-                if (index >= 0 && index < days.length) {
-                    return days[index];
-                }
-                return "";
-            }
-        });
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setGranularity(1f);//x축값 사이 간격
-
-        // Y축 설정 (완료도 퍼센트)
-        YAxis yAxisLeft = barChart.getAxisLeft();
-        yAxisLeft.setAxisMinimum(0f);
-        yAxisLeft.setAxisMaximum(100f);
-
-        // 오른쪽 Y축은 비활성화
-        YAxis yAxisRight = barChart.getAxisRight();
-        yAxisRight.setEnabled(false);
-
-        // Barchart에 데이터 설정
         barChart.setData(barData);
-        barChart.setFitBars(true);
-        barChart.getDescription().setEnabled(false);
-        barChart.setDrawGridBackground(false);
-        barChart.setDrawBorders(false);
-        barChart.animateY(1000);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setEnabled(false);
+
+        YAxis leftYAxis = barChart.getAxisLeft();
+        leftYAxis.setDrawAxisLine(false);
+        leftYAxis.setDrawGridLines(false);
+        leftYAxis.setDrawLabels(false);
+
+        YAxis rightYAxis = barChart.getAxisRight();
+        rightYAxis.setEnabled(false);
+
+        barChart.getDescription().setEnabled(false);
+        barChart.setDrawValueAboveBar(false);
+        barChart.setDrawGridBackground(false);
+        barChart.setTouchEnabled(false);
+
+        // 막대 차트 회전 및 크기 조정
+        barChart.setRotation(-90);
+        barChart.getLayoutParams().width = (int) (barWidth * completionPercentages.size() * 200); // 적절한 크기 조정
+
+        barChart.invalidate();
     }
+    public float completePercent() {
+        //임의 값을 넣음
+        int minKcal = 2000; // 최소 칼로리
+        int maxKcal = 2300; // 최대 칼로리
+
+        Random random = new Random();
+        int randomKcal = random.nextInt(maxKcal - minKcal + 1) + minKcal;
+
+        float percent = (float) randomKcal / maxKcal * 100;
+        return percent;
+    }
+
 }
